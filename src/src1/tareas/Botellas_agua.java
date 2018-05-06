24110707dd9f1f6ab89e76bfc98686d51d8bd3b0
Ivonne Duron <ivonne852>
2018-05-06 15:30:22 -0500

    botellas_agua

---
+++ src/src1/tareas/Botellas_agua.java
@@ -0,0 +1,18 @@
+package src1.tareas;
+
+import libs.Input;
+
+public class Botellas_agua {
+
+	public static void main(String[] args) {
+		System.out.println("How long was your shower? ");
+		int i = Input.get_int();
+				time_water_bottles(i);
+	}
+	
+	public static void time_water_bottles(int time) {
+		int bottle=12;
+		int result=time*bottle;
+		System.out.println(time + " minute(s) equal to "+ result +" water bottle(s)");
+	}
+}
