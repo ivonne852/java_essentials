892e39dbc9c994e0c55544fd928b98d212fff3c2
Ivonne Duron <ivonne852>
2018-05-08 20:47:49 -0500

    Implement mario facil

--- src/src1/tareas/Mario_facil.java
+++ src/src1/tareas/Mario_facil.java
@@ -7,7 +7,13 @@ public class Mario_facil {
 	public static void main(String[] args) {
 		System.out.println("How many level has the pyramid?");
 		int i = Input.get_int();
-				make_pryramid(i);
+		
+		while (i < 2 || i > 25) {
+			System.out.println("How many level has the pyramid between 2 and 25?");
+			i = Input.get_int(); 
+		}
+		
+		make_pryramid(i);
 
 	}
 	
@@ -17,7 +23,7 @@ public class Mario_facil {
 			for (int j=0; j<level-i; j++)
 				System.out.print(" ");
 			
-			for(int k=0; k<i; k++)
+			for(int k=0; k<=i; k++)
 				System.out.print("#");
 			
 			System.out.println();
