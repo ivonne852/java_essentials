96f697fd40758f0294ea283693c7b086ebf4ee9b
Ivonne Duron <ivonne852>
2018-05-09 20:54:31 -0500

    Implement Botellas_agua.java

--- src/src1/tareas/Botellas_agua.java
+++ src/src1/tareas/Botellas_agua.java
@@ -7,7 +7,13 @@ public class Botellas_agua {
 	public static void main(String[] args) {
 		System.out.println("How long was your shower? ");
 		int i = Input.get_int();
-				time_water_bottles(i);
+		
+		while (i <= 0 ) {
+			System.out.println("Please enter a valid number of minutes");
+			i = Input.get_int(); 
+		}
+		
+		time_water_bottles(i);
 	}
 	
 	public static void time_water_bottles(int time) {
